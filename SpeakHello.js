b84d2ea10e54409a7664f5c83e044162ce593737
Ligia Santos <ligia.baptista.santos@gmail.com>
2021-06-16 21:13:04 +0100

    completed step 5

--- SpeakHello.js
+++ SpeakHello.js
@@ -9,11 +9,8 @@
     console.log(speakWord + " " + name);
   };
 
-  window.helloGreeting = helloSpeaker;
+  window.helloSpeaker = helloSpeaker;
 
-  // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
-  // 'helloSpeaker' on the global scope as well.
-  // See Lecture 52, part 2
   // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
   // xxxx.xxxx = helloSpeaker;
 })(window);
